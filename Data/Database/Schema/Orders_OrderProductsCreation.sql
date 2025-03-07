8b625438814f4d0aa3d8b2aed3f7a3d02b3c4acb
MrTakuah <83469242+MrTakuah@users.noreply.github.com>
2025-03-07 06:46:11 -0500

    including sql scripts for Orders and OrderProducts schema

--- Data/Database/Schema/Orders_OrderProductsCreation.sql
+++ Data/Database/Schema/Orders_OrderProductsCreation.sql
@@ -8,3 +8,25 @@ CREATE TABLE Orders
 ALTER TABLE [dbo].[Orders]
     ADD [Status] VARCHAR(250) NULL
 GO
+
+INSERT INTO [takpos].[dbo].[Orders]
+(
+    [OrderId],
+    [BusinessDate],
+    [CreatedAt],
+    [Status]
+)
+VALUES  
+('','','','')
+
+CREATE TABLE OrderProducts
+(
+    OrderProductId UNIQUEIDENTIFIER PRIMARY KEY DEFAULT NEWID(),
+    OrderId UNIQUEIDENTIFIER FOREIGN KEY REFERENCES [takpos].[dbo].[Orders](OrderId),
+    ProductDescription VARCHAR(100),
+    Quantity INT,
+    Size VARCHAR(100),
+    Price DECIMAL(18,2),
+    BusinessDate DATETIME,
+    CreatedAt DATETIME DEFAULT GETDATE()
+)
